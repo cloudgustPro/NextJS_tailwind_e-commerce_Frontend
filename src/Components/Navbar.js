c6d4925ec0bcd2b0d65e9f7190968d93c6dd5827
cloudgustPro <cloudgust.pro@gmail.com>
2024-04-22 23:07:13 +0300

    animated navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
     <div className="hidden lg:block">
       <div className='container'>
         <div className="flex w-fit mx-auto gap-10 py-4 font-medium text-blakish">
-            <div>PRODUCTS</div>
-            <div>CATEGORIES</div>
-            <div>{`ACESSORIES`}</div>
-            <div>{`COMPUTERS`}</div>
-            <div>ABOUT US</div>
-            <div>CONTACT US</div>
+            <div className='navbar__link relative cursor-pointer'>PRODUCTS</div>
+            <div className='navbar__link relative cursor-pointer'>CATEGORIES</div>
+            <div className='navbar__link relative cursor-pointer'>{`ACESSORIES`}</div>
+            <div className='navbar__link relative cursor-pointer'>{`COMPUTERS`}</div>
+            <div className='navbar__link relative cursor-pointer'>ABOUT US</div>
+            <div className='navbar__link relative cursor-pointer'>CONTACT US</div>
         </div>
       </div>
     </div>
